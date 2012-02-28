2e6d79b6ce2f0925d3710d8d09f49e9d399c71a5
William Kral <william@kinjoe.com>
2012-02-27 22:53:53 -0800

    Adding in a readme and changing the doc in the source to match

---
+++ README.md
@@ -0,0 +1,18 @@
+Nosy
+====
+
+Nosy will watch your project's source folder and execute a command of your
+choosing whenever a watched source file changes
+
+
+Config file presedence:
+-----------------------
+
+    `pwd`/.nosy
+    ~/.nosy
+
+JSON Config example:
+--------------------
+
+    { "exec": "nosetests",
+      "watch": ["*.py", "*.html", "*.js"] }
--- nosy.py
+++ nosy.py
@@ -8,17 +8,25 @@ import sys
 import fnmatch
 import json
 
-''' Nosy will watch your data source directory for source files and execute
-    a command of your choosing when ever a watched source file changes
+'''
+Nosy
+====
+
+Nosy will watch your project's source folder and execute a command of your
+choosing whenever a watched source file changes.
 
 
-    Config file:
-        `pwd`/.nosy
-        ~/.nosy
+Config file presedence:
+-----------------------
 
-    JSON Config example:
-        { "exec": "nosetests",
-          "watch": ["*.py", "*.html", "*.js"] }
+    `pwd`/.nosy
+    ~/.nosy
+
+JSON Config example:
+--------------------
+
+    { "exec": "nosetests",
+      "watch": ["*.py", "*.html", "*.js"] }
 '''
 
 
